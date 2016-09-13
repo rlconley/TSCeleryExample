dc1ca0e99080b1123a8bcac08c32d7378dca3680
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2016-09-12 23:17:01 -0400

    refactor links to select only a tags with href

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import requests
 
 from bs4 import BeautifulSoup
-from CeleryExample.celery import app
+from celery import app
 
 
 @app.task
@@ -12,8 +12,7 @@ def song_links():
                             'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'})
     # have to mimic google bot here
     parsed_content = BeautifulSoup(page.content, 'html.parser')
-    links = parsed_content.find_all('a')
-    #TODO this needs to exclude hrefs that aren't lyrics but it doesn't
+    links = parsed_content.find(id='listAlbum').find_all('a', href=True)
     links_list = [link.get('href') for link in links]
     actual_links = [
         urljoin('http://www.azlyrics.com/lyrics/taylorswift.html', link) for
