932cfde1594e356ee9ed5159eb7c7c127bf9c02f
Mark Lavin <mlavin@caktusgroup.com>
2016-09-13 21:50:31 -0400

    Some clean up to the configuration to match the slide content.

--- celeryapp.py
+++ celeryapp.py
@@ -1,15 +1,13 @@
-from __future__ import absolute_import
-
 from celery import Celery
 
-app = Celery('CeleryExample',
-             broker='amqp://',
-             backend='rpc://',
-             include=['CeleryExample.tasks'])
+
+app = Celery('tswift', broker='amqp://guest:guest@localhost:5672//', include=['tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=3600,
+    CELERY_IGNORE_RESULT=True,
+    CELERY_TASK_SERIALIZER='json',
+    CELERY_ACCEPT_CONTENT=['json'],
 )
 
 if __name__ == '__main__':
