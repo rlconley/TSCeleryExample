727bede1c1d140126d0a9a290237d66d8038efed
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2016-09-14 18:52:37 -0400

    remove .text

--- tasks.py
+++ tasks.py
@@ -9,9 +9,7 @@ from celeryapp import app
 
 @app.task
 def song_links():
-    page = requests.get('http://www.azlyrics.com/t/taylorswift.html',
-                        headers={
-                            'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'})
+    page = requests.get('http://www.azlyrics.com/t/taylorswift.html', headers={'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'})
     # have to mimic google bot here
     parsed_content = BeautifulSoup(page.content, 'html.parser')
     links = parsed_content.find(id='listAlbum').find_all('a', href=True)
@@ -27,8 +25,8 @@ def song_lyrics(url, title):
     # have to mimic google bot here
     parsed_content = BeautifulSoup(page.text.replace('<br>', '').encode('utf-8'), 'html.parser')
     # parent_div = parsed_content.find("div", {"class": "main-page"})
-    lyrics = parsed_content.find('div', {'class': 'ringtone'}).find_next_sibling('div')
+    lyrics = parsed_content.find('div', {'class': 'ringtone'}).find_next_sibling('div').text
     saved_lyrics = open("lyrics.txt", "w")
-    saved_lyrics.write(title.text)
-    saved_lyrics.write(lyrics.text)
+    saved_lyrics.write(title)
+    saved_lyrics.write(lyrics)
     saved_lyrics.close()
